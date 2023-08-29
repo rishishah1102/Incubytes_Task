73cb53b952d07652e3aa363a504d492d9fdb59a1
rishishah1102 <rishi.shah545@gmail.com>
2023-08-29 19:17:23 +0530

    added topDown Movement, modification in leftright movement and forwardBackward movement

--- task.test.js
+++ task.test.js
@@ -1,55 +1,92 @@
-function forwardBackward(positions, direction) {
+function forwardBackward(positions, direction, steps) {
   const [x, y, z] = positions;
 
   switch (direction) {
     case "N":
-      return [x, y + 1, z];
+      return [x, y + steps, z];
     case "S":
-      return [x, y - 1, z];
+      return [x, y - steps, z];
     case "E":
-      return [x + 1, y, z];
+      return [x + steps, y, z];
     case "W":
-      return [x - 1, y, z];
+      return [x - steps, y, z];
     case "Up":
-      return [x, y, z + 1];
+      return [x, y, z + steps];
     case "Down":
-      return [x, y, z - 1];
+      return [x, y, z - steps];
     default:
-      return startingPosition;
+      return positions;
   }
 }
 
 function leftRight(currentDirection, turnDirection) {
   const directions = ["N", "E", "S", "W"];
-  const currentIndex = directions.indexOf(currentDirection);
+  const currentIndex =
+    currentDirection === "Up" || currentDirection === "Down"
+      ? directions.indexOf(prevDir)
+      : directions.indexOf(currentDirection);
 
-  const changeInDirection = turnDirection === "left" ? -1 : 1;
-  const newIndex = (currentIndex + changeInDirection + 4) % 4;
+  if (currentIndex === 0 && turnDirection === "left") {
+    newIndex = 3;
+  } else if (currentIndex === 3 && turnDirection === "right") {
+    newIndex = 0;
+  } else {
+    const changeInDirection = turnDirection === "left" ? -1 : 1;
+    newIndex = currentIndex + changeInDirection;
+  }
 
   return directions[newIndex];
 }
 
-const initialPosition = [0, 0, 0];
-const initialDirection = "E";
+let prevDir;
+function upDown(initialDirection, turnDirection) {
+  if (initialDirection !== "Up" && initialDirection !== "Down") {
+    prevDir = initialDirection;
+  }
+  return turnDirection === "Up" ? "Up" : "Down";
+}
 
 describe("Chandrayaan 3 Commands", () => {
-//   test("forward backward execution", () => {
-//     const expectedFinalPosition = [1, 0, 0];
+  const initialPosition = [0, 0, 0];
+  const initialDirection = "W";
 
-//     let forwardBackwardResult = forwardBackward(
-//       initialPosition,
-//       initialDirection
-//     );
+  test("forward execution", () => {
+    const expectedFinalPosition = [-1, 0, 0];
 
-//     expect(forwardBackwardResult).toEqual(expectedFinalPosition);
-//   });
+    let forwardBackwardResult = forwardBackward(
+      initialPosition,
+      initialDirection,
+      1
+    );
+
+    expect(forwardBackwardResult).toEqual(expectedFinalPosition);
+  });
+
+  test("backward execution", () => {
+    const expectedFinalPosition = [1, 0, 0];
+
+    let forwardBackwardResult = forwardBackward(
+      initialPosition,
+      initialDirection,
+      -1
+    );
+
+    expect(forwardBackwardResult).toEqual(expectedFinalPosition);
+  });
 
   test("left right execution", () => {
-    // const direction = "E";
     const expectedDirection = "N";
 
-    const leftRightResult = leftRight(initialDirection, "left");
+    const leftRightResult = leftRight(initialDirection, "right");
 
     expect(leftRightResult).toEqual(expectedDirection);
+  });
+
+  test("up down execution", () => {
+    const expectedDirection = "Up";
+
+    const upDownResult = upDown(initialDirection, "Up");
+
+    expect(upDownResult).toEqual(expectedDirection);
   });
 });
